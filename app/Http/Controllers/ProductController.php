b646fcdff12c9492f819829814c06656a304644c
hanan467 <hananansaresmael@gmail.com>
2025-02-20 21:39:32 +0300

    added packages

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+use function Pest\Laravel\json;
+
+class ProductController extends Controller
+{
+   
+}
