e954effacf976c0a2cff16b5b9a800621dac82cd
hanan467 <hananansaresmael@gmail.com>
2025-02-22 20:10:10 +0300

    added breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ScrapeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/addProduct', [ScrapeController::class, 'showForm'])->name('product.add');
+    Route::post('/addProduct', [ScrapeController::class, 'store'])->name('product.store');
+
+
+});
+
+require __DIR__.'/auth.php';
